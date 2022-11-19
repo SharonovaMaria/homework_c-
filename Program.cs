51c8e6f0a968df676522459af3e9e33a98391b13
Sharonova Maria <shar10@bk.ru>
2022-11-20 01:29:17 +0300

    new dz

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+// Console.WriteLine("Введите количество чисел");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int count = 0;
+// int[] array = GetArray(size);
+
+// int[] GetArray(int size){
+//     int[] res = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         res[i] = Convert.ToInt32(Console.ReadLine());
+//         if(res[i]>0){
+//             count++;
+//         }
+//     }
+//     return res;
+// }
+
+// Console.WriteLine(String.Join(",",array));
+// Console.WriteLine("чисел больше 0 : " + count);
+
+
+
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+//  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+//   значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// Console.WriteLine("Введите число");
+// double b1 = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// double k1 = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// double b2 = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// double k2 = Convert.ToDouble(Console.ReadLine());
+
+// if(k1 == k2){
+//     Console.WriteLine("Параллельны");
+// }else{
+//     double x = (b2-b1)/(k1-k2);
+//     double y = k1 * x + b1;
+//     Console.WriteLine(x +";"+ y);
+// }
+
+
+
+
+
+
